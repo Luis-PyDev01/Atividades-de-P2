05a2ca1c280f5ce82d209800810be0beff872c78
Luis-PyDev01 <lm0407937@gmail.com>
2026-04-16 22:51:37 -0300

    Add files via upload
    feito

---
+++ Exercicio3.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+
+public class Exercicio3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o tipo de combustivel");
+        String combustivel = sc.nextLine();
+        System.out.println("Digite a quantidade de combustivel");
+        double quantidade = sc.nextDouble();
+
+        if (quantidade >20){
+            if (combustivel.equalsIgnoreCase("gasolina")){
+                double valor = quantidade * 6.00;
+                double desconto = quantidade * 0.06;
+                double valorFinal = valor - desconto;
+                System.out.println("Valor final a pagar  " + valorFinal);
+            } else if (combustivel.equalsIgnoreCase("alcool")){
+                double valor = quantidade * 4.50;
+                double desconto = valor * 0.05;
+                double valorFinal = valor - desconto;
+                System.out.println("Valor a pagar: " + valorFinal);
+            }
+            else {
+                System.out.println("Tipo de combustivel invalido");
+            }
+        }
+        else {
+            if (combustivel.equalsIgnoreCase("gasolina")){
+                double valor = quantidade * 6.00;
+                double desconto = valor * 0.03;
+                double valorFinal = valor - desconto;
+                System.out.println("Valor a pagar: " + valorFinal);
+            } else if (combustivel.equalsIgnoreCase("alcool")){
+                double valor = quantidade * 4.50;
+                double desconto = valor * 0.04;
+                double valorFinal = valor - desconto;
+                System.out.println("Valor a pagar: " + valorFinal);
+            } else {
+                System.out.println("Tipo de combustivel invalido");
+            }
+        }
+
+    }
+}
---
+++ Exercicio4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Exercicio4 {
+
+    public static void main(String[] args) {
+
+        for (int i = 10; i>= 0 ; i--){
+            System.out.println("contagem :" +i);
+            if(i == 0){
+                System.out.println("Lancar!");
+            }
+
+        }
+    }
+}
